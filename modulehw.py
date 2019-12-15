bda8e5368327cd67ecb6021c0a55b584df8f3fd5
mhutch2b87 <mhutch2b87@gmail.com>
2019-12-14 23:14:21 -0600

    first commit

---
+++ modulehw.py
@@ -0,0 +1,10 @@
+from IPython.display import clear_output
+from math import pi
+
+def squareft_Calc(length, width):
+    clear_output()
+    print(length * width)
+
+def circumference_Calc(radius):
+    clear_output()
+    print(2 * pi * radius)
